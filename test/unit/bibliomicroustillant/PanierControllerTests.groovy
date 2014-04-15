8a9d02024670bf5504ed859620e097e5488981e4
hordesk <mathias.faure.31@gmail.com>
2014-04-15 18:47:35 +0200

    Tests unitaires

---
+++ test/unit/bibliomicroustillant/PanierControllerTests.groovy
@@ -0,0 +1,17 @@
+package bibliomicroustillant
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(PanierController)
+class PanierControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
---
+++ test/unit/bibliomicroustillant/PanierServiceTests.groovy
@@ -0,0 +1,17 @@
+package bibliomicroustillant
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PanierService)
+class PanierServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
